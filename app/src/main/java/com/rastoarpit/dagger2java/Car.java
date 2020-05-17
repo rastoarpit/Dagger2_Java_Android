0aee953c29b69fa7de09bfb9b51b5b740919d57b
Arpit Rastogi <arpit.rastogi@yahoo.com>
2020-05-17 21:58:26 +0530

    Dagger 2 Tutorial Part 4 - METHOD INJECTION

--- app/src/main/java/com/rastoarpit/dagger2java/Car.java
+++ app/src/main/java/com/rastoarpit/dagger2java/Car.java
@@ -7,13 +7,18 @@ import javax.inject.Inject;
 public class Car {
     private static final String TAG = "Car";
 
-     private Engine engine;
+     @Inject Engine engine; //2. Field injection
      private Wheels wheels;
 
      @Inject
-    public Car(Engine engine, Wheels wheels) {
-        this.engine = engine;
+    public Car(Wheels wheels) {//1. Constructor injection
+         //If we have constructor injection - Field and Method are also injected automatically
         this.wheels = wheels;
+    }
+
+    @Inject
+    public void enableRemote(Remote remote){//3. Method injection
+         remote.setListener(this);
     }
 
     public void drive(){
